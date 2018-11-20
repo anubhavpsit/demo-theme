d5d80f87a2b2ea6e01284f4a38b30f6558c36b61
Anubhav <anubhav.psit.2009@gmail.com>
2018-11-20 23:21:22 +0530

    logout option added

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { TokenService } from '../../service/auth/token.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +13,17 @@ export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<void>();
   @Output() openSearch = new EventEmitter<void>();
   @Output() toggleFullscreen = new EventEmitter<void>();
+
+  constructor(
+    private router: Router,
+    private Auth: AuthService,
+    private Token: TokenService
+    ) {}
+
+  logOut(event: MouseEvent) {
+    event.preventDefault();
+    this.Auth.changeAuthStatus(false);
+    this.Token.remove();
+    this.router.navigateByUrl('/signin');
+  }
 }
