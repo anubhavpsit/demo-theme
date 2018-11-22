2bd8c5ef982e76c5b121f23ac7b3072fcaccf845
Anubhav <anubhav.psit.2009@gmail.com>
2018-11-22 23:53:17 +0530

    Added camera in createstore component

--- src/app/store/createstore/createstore.component.ts
+++ src/app/store/createstore/createstore.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-createstore',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatestoreComponent implements OnInit {
 
-  constructor() { }
+  public show_camera_section:boolean = true;
+  @ViewChild("video")
+  public video: ElementRef;
 
-  ngOnInit() {
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  public captures: Array<any>;
+
+  constructor() { this.captures = []; }
+
+  ngOnInit() {}
+
+  public ngAfterViewInit() {
+  //   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  //       navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+  //           // this.video.nativeElement.src = window.URL.createObjectURL(stream);
+  //           // this.video.nativeElement.play();
+  //       });
+  //   }
+    //this.show_camera_section = false;
+  }
+
+  openCamera() {
+    //this.show_camera_section = true;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+            this.video.nativeElement.src = window.URL.createObjectURL(stream);
+            this.video.nativeElement.play();
+        });
+    }
+  }
+
+  captureProductImage() {
+      var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
   }
 
 }
